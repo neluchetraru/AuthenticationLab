bddd998a7f8c8099406ff84d6b38e02fca2d6136
Chetraru Ion <s204710@student.dtu.dk>
2023-10-25 16:30:21 +0200

    Encryption class created

---
+++ src/Encryption.java
@@ -0,0 +1,22 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class Encryption {
+
+    public Encryption() {
+
+    }
+    static String hashPassword(String password) {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        byte[] encodedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
+        return Arrays.toString(encodedPassword);
+    }
+
+}
